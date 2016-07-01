16981cc52fcba4dd99e1f73cff21c0d10bd1678d
Jared Scott <jmsmm92@gmail.com>
2016-06-30 21:26:11 -0500

    renamed two files for better description. shopListFunction has a function that fails when using while($row = mysqli_fetch_assoc($result)). Currently debugging...

--- shopList.php
+++ shopList.php
@@ -1,42 +1,69 @@
 <?php
-session_start();
-include_once 'dbconnect.php';
-
-if(!isset($_SESSION['userSession']))
-{
- header("Location: index.php");
-}
-
-$query = $MySQLi_CON->query("SELECT * FROM users WHERE user_id=".$_SESSION['userSession']);
-$userRow=$query->fetch_array();
-
-//create an array of items from items table
-$itemQuery = $MySQLi_CON->query("SELECT * FROM items");
-$array = array();
-
-//look through query 
-while($itemRow = mysqli_fetch_assoc($itemQuery)){
-	//add each row returned into an array
-	$array[] = $itemRow;
-}
-
-$secondquery = "SELECT image, itemName, price, description ";
-$secondquery .= "FROM items ";
-
-$result = mysqli_query($MySQLi_CON, $secondquery);
-
-//test if the query failed
-if (!$result){
-	die("Database query failed.");
-}else{
-	echo "<p></p>";
-}
-
-//$querytwo = $MySQLi_CON->query("SELECT * FROM items");
-//echo $querytwo;
-//$userRow=$query->fetch_array();
-$MySQLi_CON->close();
+//session_start();
+include_once 'shopListFunctions.php';
 ?>
+
+<!-- Call shopList.js -->
+<!-- <script type="text/javascript" src="shopList.js"></script>-->
+
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+"http://www.w3.org/TR/html4/loose.dtd">
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
+
+<html lang="en">
+<head>
+		
+<title>Shop</title>
+<nav class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="http://www.codingcage.com">Coding Cage</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li><a href="http://www.codingcage.com/search/label/jQuery">jQuery</a></li>
+            <li><a href="http://www.codingcage.com/search/label/PHP">PHP</a></li>
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#"><span class="glyphicon glyphicon-user"></span>&nbsp; <?php require_once 'shopList.php'; echo $user; ?></a></li>
+            <li><a href="logout.php?logout"><span class="glyphicon glyphicon-log-out"></span>&nbsp; Logout</a></li>
+			<li><a href="#" id="cart"><span class="glyphicon"></span>&nbsp; Cart: </a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+</head>
+
+<body>
+
+<p  style="display: block; padding-top: 40px;"></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+
+<ul onLoad="displayItems()">
+<?php displayItems(); ?>
+</ul>
+
+<p id="notes" onLoad="buttonName()"> </p>
+
+
+</body>
+
+</html>
 
 <?php
 //mysqli_close($connection);
