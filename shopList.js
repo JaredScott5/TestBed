69576e96cb214393d3c759fce7db6ad5fc163469
Jared Scott <jmsmm92@gmail.com>
2016-07-04 19:24:13 -0500

    ShopList.js is now working with exception to the last two lines in function 'myfunction'.

--- shopList.js
+++ shopList.js
@@ -1,49 +1,26 @@
 var cartCount = 0;
 
 window.onload = function(){
-var note = document.getElementById("notes");
-note.innerHTML = "testing";
-document.getElementById("cart").innerHTML = 0;
+	var note = document.getElementById("notes");
+	note.innerHTML = "testing windows.onload from js";
+	document.getElementById("cart").innerHTML = "Cart " + 0;
 };
 
-function showUser(){
-	if (window.XMLHttpRequest) {
-            xmlhttp = new XMLHttpRequest();
-			document.getElementById("notes").innerHTML = "testing2";
-        } else {
-            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-			document.getElementById("notes").innerHTML = "testing3";
-        }
-		
-					document.getElementById("notes").innerHTML = "goint to second part";
+function buttonName(){
+	var stringData = document.getElementById("notes2");
+	stringData.innerHTML = "testing function buttonName";
+		var length = document.getElementsByTagName("LI").length;
 
-        xmlhttp.onreadystatechange = function() {
-            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-                document.getElementById("notes").innerHTML = xmlhttp.responseText;
-            }else{
-				document.getElementById("notes").innerHTML = "failed 3rd part";
-			}
-		};
-		
-		xmlhttp.open("GET","getuser.php?q="+str,true);
-        xmlhttp.send();
+	for(var i = 0; i < length; i++){
+	document.getElementsByTagName("LI")[i].setAttribute(javascript_array['item_id']);
+	}
 }
 
 function myfunction(parent){
 	document.getElementById("cart").innerHTML = ++cartCount;
 	document.getElementById("notes").innerHTML = "testing";
 
-	var itemToAddToCart = <?php $array[parent]['item_id']?>;
-		document.getElementById("notes").innerHTML = "chldNode 1 value is" + itemToAddToCart;
-}
+	var itemToAddToCart = javascript_array['item_id'];
 	
-function buttonName(){
-	
-		document.getElementById("notes2").innerHTML = "test again";
-	var length = document.getElementsByTagName("LI").length;
-
-	for(var i = 0; i < length; i++){
-		document.getElementsByTagName("LI")[i].setAttribute(<?php echo $array[i]['item_id']?>));
-		<?php echo $array[i]['item_id']?>
-	}
+	document.getElementById("notes").innerHTML = "chldNode 1 value is" + itemToAddToCart;
 }
