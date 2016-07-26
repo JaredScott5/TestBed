66a0bc3bb464dc4dfafb1ae0abb419036b159d28
Jared Scott <jmsmm92@gmail.com>
2016-07-26 18:57:08 -0500

    admin.js, admin.php, and search.php now work together to return search results correctly

--- admin.js
+++ admin.js
@@ -1,9 +1,6 @@
-window.onload = function(){
-  var searchTerm = document.getElementById("search_bar");
-};
-
-function search(searchTerm){
-	
+function search(){
+	var searchTerm = document.getElementById("search_bar").value;
+	document.getElementById("debug").innerHTML = "search bar variable is " + searchTerm;
 	httpRequest = new XMLHttpRequest();
 	httpRequest.onreadystatechange = displaySearchResult;
 	httpRequest.open('POST', 'search.php');
@@ -25,22 +22,4 @@ function displaySearchResult() {
   catch( e ) {
     alert('Caught Exception: ' + e.description);
   }
-}
-
-function goTo(){
-	
-	//check if not a number
-	if(isNaN(user)){
-		//if true, check my username
-		
-		//if result is false, check by email
-	}else{
-		
-	}
-	//if flase, seach by id
-	//else if it has a '@', check by email
-	//else then just check my username
-	
-
-	//else, return as failure aka no such userAgent
 }
