95b73a256753c08443683dd3ebc4228981de82df
Jeremy Crook <jac13b@my.fsu.edu>
2016-08-22 20:58:37 -0400

    Minor code-cosmetic changes

--- orderHistory.php
+++ orderHistory.php
@@ -5,6 +5,7 @@ include_once 'orderHistoryFunctions.php';
 
 
 <html lang="en">
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
 "http://www.w3.org/TR/html4/loose.dtd">
 <!-- Latest compiled and minified CSS -->
@@ -12,8 +13,6 @@ include_once 'orderHistoryFunctions.php';
 
 <!-- Optional theme -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
-
-
 
 <head>
 <title>Order History</title>
