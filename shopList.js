fc0e144e7a4b8e751041f34b242cd034992999db
Jared Scott <jmsmm92@gmail.com>
2016-08-29 19:53:16 -0500

    item-page now has a functioning 'add to cart' button. more formatting has been added for item-page. the shopList files had some redundent code that i removed

--- shopList.js
+++ shopList.js
@@ -4,18 +4,6 @@ window.onload = function(){
 	//document.getElementById("cart").innerHTML = "Cart " + 0;
 };
 
-/*
-function buttonName(){
-	var stringData = document.getElementById("notes2");
-	stringData.innerHTML = "testing function buttonName";
-		var length = document.getElementsByTagName("LI").length;
-
-	for(var i = 0; i < length; i++){
-	document.getElementsByTagName("LI")[i].setAttribute(javascript_array['item_id']);
-	}
-}
-*/
-
 function addToCart(parent){
 	//document.getElementById("cart").innerHTML = ++cartCount;
 	//document.getElementById("notes").innerHTML = "testing";
