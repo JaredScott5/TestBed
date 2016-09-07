67ebb9af4bed592aef7e4b544aec48e1e5db58de
Jeremy Crook <jac13b@my.fsu.edu>
2016-09-06 20:50:23 -0400

    Edited for accuracy

--- footer.php
+++ footer.php
@@ -1,10 +1,10 @@
 <nav class="navbar navbar-default navbar-fixed-bottom">
 <footer class="pull-left" style="margin-left: 30px">
-	  <div>Created by Jeremy Crooke and Jared Scott</div>
+	  <div>&copy; 2016 Jeremy Crook and Jared Scott</div>
 	  </footer>
 	  
 	  <footer class="pull-right" style="margin-right: 30px">
-	  <div>Contact Information: <a href="mailto:jeremy@example.com"> jeremy@example.com | </a> 
+	  <div>Contact Information: <a href="mailto:jeremy.crook2@gmail.com"> jeremy.crook2@gmail.com | </a> 
 		   <a href="mailto:jmsmm92@gmail.com">jmsmm92@gmail.com</a>.</div>
 	  </footer>
       
