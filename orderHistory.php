cb5baa8741cc33c013e56c24d8335eb461684928
Jeremy Crook <jac13b@my.fsu.edu>
2016-09-20 20:57:11 -0400

    orderHistory display loop updated.

--- orderHistory.php
+++ orderHistory.php
@@ -59,7 +59,7 @@ if($rowCount!=0){
   GROUP BY orderNumber
   ";
   */
-  $newResult = mysqli_query($MySQLi_CON, $query);
+  //$newResult = mysqli_query($MySQLi_CON, $query);
 
   $ordersQuery =
   "
@@ -118,96 +118,56 @@ $MySQLi_CON->close();
 </head>
 
 <body>
-  <!-- tables for past orders -->
-  <p  style='display: block; padding-top: 100px;'></p>
 
-  <?php $prev = NULL; ?>
-  <?php //$totalCost = 0; ?>
-  <?php while($itemRow = mysqli_fetch_assoc($result)): ?>
-    <?php if ($itemRow['orderNumber'] != $prev) :?>
-  
-      <table style='width:75%' align='center' cellpadding='2' cellspacing='2' border='2'>
-      <tr>
-      <?php foreach ($finfo as $val) : ?>
-        <th style='text-align:center'><?php $name = str_replace('_', ' ', $val->name); echo $name; ?></th>
-      <?php endforeach; ?>
-      </tr>
+<?php $currentOrder = 0; ?>
 
-      <?php //$totalCost = 0; ?>
-    <?php endif; ?>
-    <?php //while($itemRow = mysqli_fetch_assoc($result)): ?>
-    
-    <tr>
-      <?php foreach ($finfo as $val) : ?>
-        <th style='text-align:center'>
-          <?php
-            if ($val->name === 'image') {
-              echo "<img class=\"img-responsive\" width=\"150\" height=\"150\" src=" . $itemRow[$val->name] . "></img>";
-            } else {
-            echo $itemRow[$val->name];
-            }
-          ?>
-      <?php endforeach; ?>
-    </tr>
-    <?php
-		//$totalCost = $totalCost + ($itemRow["price"] * $itemRow["quantityOrdered"]);
-		$count = $count + 1;
-    $prev = $itemRow['orderNumber'];
-    ?>
-    <?php endwhile; ?>
-  </table>
-
-
-
-<!-- Bootstrap/CSS Work In Progress
-
-<div class="order">
-  <div class="summary row">
-    <div class="col-xs-2">
-      <p class="order number"><?php //echo $itemRow['orderNumber'] ?></p>
+<?php while($itemRow = mysqli_fetch_assoc($result)) : ?>
+  <?php if ($itemRow['orderNumber'] !== $currentOrder) : ?>
+    <div class="order">
+      <div class="summary row">
+        <div class="col-xs-2">
+          <p class="order number"><?php echo $itemRow['orderNumber'] ?></p>
+        </div>
+        <div class="col-xs-2">
+          <p class="order date"><?php echo $itemRow['orderDate'] ?></p>
+        </div>
+        <div class="col-xs-2">
+          <p class="shipped date"><?php echo $itemRow['shippedDate'] ?></p>
+        </div>
+        <div class="col-xs-2">
+          <p class="total cost"><?php echo $itemRow['total'] ?></p>
+        </div>
+        <div class="col-xs-2">
+          <p class="comments"><?php echo $itemRow['comments'] ?></p>
+        </div>
+        <div class="col-xs-2">
+          <p class="whitepace">&nbsp;</p>
+        </div>  
+      </div>
+  <?php endif; ?>
+      <div class="detail row">
+        <div class="col-xs-2">
+          <p class="whitepace">&nbsp;</p>
+        </div>    
+        <div class="col-xs-2">
+          <p class="item img"><?php echo "<img class=\"img-responsive\" width=\"150\" height=\"150\" src=" . $itemRow['image'] . "></img>"; ?></p>
+        </div>
+        <div class="col-xs-2">
+          <p class="item name"><?php echo $itemRow['itemName'] ?></p>
+        </div>
+        <div class="col-xs-2">
+          <p class="quantity ordered"><?php echo $itemRow['quantityOrdered'] ?></p>
+        </div>
+        <div class="col-xs-2">
+          <p class="price per item"><?php echo $itemRow['price'] ?></p>
+        </div>
+        <div class="col-xs-2">
+          <p class="total item price"><?php echo $totalItemCost = $itemRow['price'] * $itemRow['quantityOrdered'] ?></p>
+        </div>    
+      </div>
     </div>
-    <div class="col-xs-2">
-      <p class="order date"><?php //echo $itemRow['orderDate'] ?></p>
-    </div>
-    <div class="col-xs-2">
-      <p class="shipped date"><?php //echo $itemRow['shippedDate'] ?></p>
-    </div>
-    <div class="col-xs-2">
-      <p class="total cost"><?php //echo $itemRow['total'] ?></p>
-    </div>
-    <div class="col-xs-2">
-      <p class="comments"><?php //echo $itemRow['comments'] ?></p>
-    </div>
-    <div class="col-xs-2">
-      <p class="whitepace">&nbsp;</p>
-    </div>  
-  </div>
-  <div class="detail row">
-    <div class="col-xs-2">
-      <p class="whitepace">&nbsp;</p>
-    </div>    
-    <div class="col-xs-2">
-      <p class="item img"><?php //echo "<img class=\"img-responsive\" width=\"150\" height=\"150\" src=" . $itemRow[$val->name] . "></img>"; ?></p>
-    </div>
-    <div class="col-xs-2">
-      <p class="item name"><?php //echo $itemRow['itemName'] ?></p>
-    </div>
-    <div class="col-xs-2">
-      <p class="quantity ordered"><?php //echo $itemRow['quantityOrdered'] ?></p>
-    </div>
-    <div class="col-xs-2">
-      <p class="price per item"><?php //echo $itemRow['price'] ?></p>
-    </div>
-    <div class="col-xs-2">
-      <p class="total item price"><?php //echo $totalItemPrice ?></p>
-    </div>    
-  </div>
-  
--->
-<?php //foreach ($orders as $val1) : ?>
-  <?php //foreach $details as $val2) : ?>
-  <?php //endforeach; ?>
-<?php //endforeach; ?>
+  <?php $currentOrder = $itemRow['orderNumber']; ?>
+<?php endwhile; ?>
 
 <div id="footer"><?php include_once 'footer.php'; ?></div>
 
