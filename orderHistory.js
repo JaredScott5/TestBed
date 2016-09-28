12aafcb5523fabf81f72eaad7def77d8a915df4d
Jeremy Crook <jac13b@my.fsu.edu>
2016-09-27 21:27:51 -0400

    More jQuery and CSS updates

--- orderHistory.js
+++ orderHistory.js
@@ -1,5 +1,15 @@
 $(document).ready(function(){
-  $('.order').click(function() {
-    $(this).children('.details').toggle();
+  $('.exp-col').text(function () {
+    $details = $(this).parents('.order').children('.details');
+          return $details.is(":visible") ? "Hide Details" : "Show Details";
+      });
+  $('.exp-col').click(function() {
+    $header = $(this);
+    $details = $(this).parents('.order').children('.details');
+    $details.toggle(0, function () {
+      $header.text(function () {
+          return $details.is(":visible") ? "Hide Details" : "Show Details";
+      });
+    });
   });
 });
