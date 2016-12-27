68a650f4a046f0bc936a03d5896b8eda0142cbd8
Jared Scott <jmsmm92@gmail.com>
2016-12-26 19:16:27 -0600

    search algorithms for admin searches have been completely moved to admin-order-search.php

--- adminsearch.js
+++ adminsearch.js
@@ -1,3 +1,30 @@
+
+/*$(document).ready(function(){
+
+  $("#search_string").click(function () { 
+  alert($('input[name=radioGroup]').val());
+	var radioVal = $('input[name=radioGroup]:checked', '#myForm').val();
+	var searchTerm = document.getElementById("search_bar").value;
+
+    // console.log(item_id);
+    // console.log(quantity);
+    $.ajax({
+      url: "adminsearch.php",
+      type: "POST",
+      cache: false,
+      dataType: 'json',
+      data: {searchTerm: searchTerm, radioVal: radioVal},
+      success: function(data){
+        $('#debug').html("Success");
+      },
+      error: function(data){
+        console.log(data.msg);
+       $('#debug').html("Failed with " + radioVal + "in data");
+      }
+    });
+  });
+});*/
+
 function search(){
 	var searchTerm = document.getElementById("search_bar").value;
 	var radioElements = document.getElementsByName('radioGroup');
@@ -16,7 +43,7 @@ function search(){
 		}	
 	}
 	
-	document.getElementById("debug").innerHTML = "radio variable is " + radioSelected + " new var is " + searchTerm;
+	//document.getElementById("debug").innerHTML = "radio variable is " + radioSelected + " new var is " + searchTerm;
 	httpRequest = new XMLHttpRequest();
 	httpRequest.onreadystatechange = displaySearchResult;
 	httpRequest.open('POST', 'adminsearch.php');
