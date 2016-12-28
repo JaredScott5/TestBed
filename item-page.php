ae3117715fb4fbbb744558dff64a1ad539ce677c
Jared Scott <jmsmm92@gmail.com>
2016-12-28 07:37:27 -0600

    seperated css in item-page and shopList into thier respective CSS files

--- item-page.php
+++ item-page.php
@@ -30,15 +30,17 @@ $item_desc = $row['description'];
  <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
 "http://www.w3.org/TR/html4/loose.dtd">
 <!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" 
+integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 
 <!-- Optional theme -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" 
+integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
 
 
 <html lang="en">
 <head>
-<link rel="stylesheet" type="text/css" href="styles.css">
+<link rel="stylesheet" type="text/css" href="item-page.css">
 <link rel="stylesheet" type="text/css" href="footer.css">
 
 <title><?php echo $item_name ?></title>
@@ -55,22 +57,24 @@ $item_desc = $row['description'];
 	<div class="container" id="topContainer">
 		<div class="row">
 			<div id="firstCol" class="col-lg-10 col-md-10 col-sm-10" style="">
-				<div id="item-image" style="position: relative;">
+				<div id="item-image">
 					<a href="item-page.php?item_id=<?php echo $row["item_id"]; ?>">
-					<img class="" 
-					width="200" 
-					height="200"  
+					<img 
 					src=<?php echo $row["image"]; ?> 
 					id='image'></a>
+					<!--class="" 
+					width="200" 
+					height="200"-->  
+					
 					</img>
 				</div>
 			</div>
 
 			<div id="thirdCol" class="col-lg-2 col-md-2 col-sm-2" style="">
-				<div id="item-price" class="" style="position: relative; font-size:20px;">
-					<p style="">$<?php echo $item_price ?></p>
+				<div id="item-price" class=""> <!--style="position: relative; font-size:20px;"-->
+					<p>$<?php echo $item_price ?></p>
 				</div>
-				<div class="item-add-to-cart" style="padding-top: 120px; right: 0;" id=<?php echo $item_id?>>
+				<div id=<?php echo $item_id?>class="item-add-to-cart"> <!--style="padding-top: 120px; right: 0;"-->
 					<a class='btn btn-lg btn-primary' href='#' role='button' onClick='addToCart(<?php echo $row["item_id"]; ?>)'>Add To Cart</a>
 				</div>
 			</div>
@@ -79,7 +83,7 @@ $item_desc = $row['description'];
   
   <hr></hr>
 	<div class="container" id="midContainer">
-		<div class="" style="position: relative; font-size:20px;"> 
+		<div id="sub-container" style=""> 
 			<h3> About This Item:</h3>
 				<div id="features">
 					<p style="border-left: 6px solid red;"><?php echo $item_desc; ?></p>
@@ -89,15 +93,15 @@ $item_desc = $row['description'];
 	</div>
 	<hr></hr>
 	<div class="container" id="midContainer">
-		<div id="item-id" class="" style="position: relative; font-size:20px;">
+		<div id="item-id" class="">
 			<h3> Item ID#: </h3>
-			<p style="border-left: 6px solid red;"><?php echo $item_id ?></p>
+			<p id="item-id-border" style="border-left: 6px solid red;"><?php echo $item_id ?></p>
 		</div>
 		<br>
 	</div>
 	<hr></hr>
 	<div class="container" id="midContainer">
-		<div id="item-desc" class="" style="position: relative; font-size:20px;">
+		<div id="item-desc" class="" >
 			<h3> Description: <br></h3>
 			<p style="border-left: 6px solid red;"><?php echo $item_desc ?></p>
 		</div>
