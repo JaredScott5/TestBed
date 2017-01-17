1e46dcf9ed5a58415297a2f875e14ae6f508af31
Jeremy Crook <jac13b@my.fsu.edu>
2017-01-16 20:52:04 -0500

    Updated home.php with new style template.

--- footer.php
+++ footer.php
@@ -1,12 +1,16 @@
-<nav class="navbar navbar-default navbar-static-bottom">
-	<div id="parent" class="">
-		<footer class="pull-left" style="">
-		<div>&copy; 2016 Jeremy Crook and Jared Scott</div>
-		</footer>
-	  
-		<footer class="pull-right" style="">
-		<div>Contact Information: <a href="mailto:jeremy.crook2@gmail.com"> jeremy.crook2@gmail.com</a> |
-			<a href="mailto:jmsmm92@gmail.com">jmsmm92@gmail.com</a></div>
-		</footer>
+<div class="container">
+	<footer>
+    <div class="row">
+      <div class="col-lg-12">
+        <div class="pull-left">
+          <div>&copy; 2016 Jeremy Crook and Jared Scott</div>
+        </div>
+        
+        <div class="pull-right">
+          <div>Contact Information: <a href="mailto:jeremy.crook2@gmail.com"> jeremy.crook2@gmail.com</a> |
+          <a href="mailto:jmsmm92@gmail.com">jmsmm92@gmail.com</a></div>
+        </div>
+      </div>
+    </footer>
 	</div>      
-</nav>
+</div>
