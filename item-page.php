9da21ee139982e67affad1a29f0752c5c6982d3e
Jeremy Crook <jac13b@my.fsu.edu>
2017-01-17 20:56:51 -0500

    Updated to use template

--- item-page.php
+++ item-page.php
@@ -12,7 +12,8 @@ $find_item = $MySQLi_CON->query(
     WHERE item_id='$item_id'"
   );
   
-  $row = mysqli_fetch_assoc($find_item);
+$row = mysqli_fetch_assoc($find_item);
+
 $item_id = $row['item_id'];
 $item_name = $row['itemName'];
 $item_price = $row['price'];
@@ -35,79 +36,131 @@ $item_desc = $row['description'];
 <head>
 	<link rel="stylesheet" href="css/bootstrap.min.css">
 
-<link rel="stylesheet" type="text/css" href="item-page.css">
-<link rel="stylesheet" type="text/css" href="footer.css">
+  <link rel="stylesheet" type="text/css" href="item-page.css">
+  <link rel="stylesheet" type="text/css" href="footer.css">
 
-<title><?php echo $item_name ?></title>
+  <title><?php echo $item_name; ?></title>
 </head>
 
 <body>
+  <!-- Navigation -->
+  <?php include ('navbar.php'); ?>
+  <p  style="display: block; padding-top: 50px;"></p>
 
-<p  style="display: block; padding-top: 50px;"></p>
+ <!-- Page Content -->
+  <div class="container">
 
-<div id="page">
-	<div class="container" id="leftAdContainer"> This is where an ad would go</div>
+      <div class="row">
 
-	<div class="container" id="itemDetailContainer">
+          <div class="col-md-3">
+              <p class="lead">Shop Name</p>
+              <div class="list-group">
+                  <a href="#" class="list-group-item active">Category 1</a>
+                  <a href="#" class="list-group-item">Category 2</a>
+                  <a href="#" class="list-group-item">Category 3</a>
+              </div>
+          </div>
 
-		<div class="container" id="topContainer">
-			<h1><?php echo $item_name ?> </h1>
-				<div class="row">
-					<div id="firstCol" class="col-lg-10 col-md-10 col-sm-10" style="">
-						<div id="item-image">
-							<a href="item-page.php?item_id=<?php echo $row["item_id"]; ?>">
-							<img 
-							src=<?php echo $row["image"]; ?> 
-							id='image'></a>
-							</img>
-						</div>
-						<hr></hr>
-						<div class="container" id="midContainer">
-							<div id="sub-container" style=""> 
-								<h3> About This Item:</h3>
-									<div id="features">
-										<p style="border-left: 6px solid red;"><?php echo $item_desc; ?></p>
-									</div>
-									<br>
-							</div>
-						</div>
-		
-						<hr></hr>
-						<div class="container" id="midContainer">
-							<div id="item-id" class="">
-								<h3> Item ID#: </h3>
-								<p id="item-id-border" style="border-left: 6px solid red;"><?php echo $item_id ?></p>
-							</div>
-							<br>
-						</div>
-		
-						<hr></hr>
-						<div class="container" id="midContainer">
-							<div id="item-desc" class="" >
-								<h3> Description: <br></h3>
-								<p style="border-left: 6px solid red;"><?php echo $item_desc ?></p>
-							</div>
-							<br>
-						</div>
-					</div>
+          <div class="col-md-9">
 
-					<div id="thirdCol" class="col-lg-2 col-md-2 col-sm-2" style="">
-						<div id="item-price" class=""> <!--style="position: relative; font-size:20px;"-->
-							<p>$<?php echo $item_price ?></p>
-						</div>
-						<div id=<?php echo $item_id?>class="item-add-to-cart"> <!--style="padding-top: 120px; right: 0;"-->
-							<a class='btn btn-lg btn-primary' href='#' role='button' onClick='addToCart(<?php echo $row["item_id"]; ?>)'>Add To Cart</a>
-						</div>
-					</div>				
-				</div>  		
-		</div>	
-	</div>
- </div>
-<ul></ul>
+            <div class="item" id=<?php echo $row["item_id"]; ?>>
+              <a href="item-page.php?item_id=<?php echo $row["item_id"]; ?>">
+              <img class="img-responsive"
+              src=<?php echo $row["image"]; ?> 
+              id='image'></a>
+              </img>
+              <div class="caption-full" id=<?php echo $row["item_id"]; ?>>
+                <h4 class="pull-right">$<?php echo $item_price; ?></h4>
+                <h4><a href="#"><?php echo $item_name; ?></a>
+                </h4>
+                <p><?php echo $item_desc; ?></p>
+                <div id=<?php echo $item_id; ?> class="add-to-cart">
+                  <a class='btn btn-lg btn-primary' href='#' role='button'>Add To Cart</a>
+                </div>
+              </div>
+              <div class="ratings">
+                <p class="pull-right">3 reviews</p>
+                <p>
+                    <span class="glyphicon glyphicon-star"></span>
+                    <span class="glyphicon glyphicon-star"></span>
+                    <span class="glyphicon glyphicon-star"></span>
+                    <span class="glyphicon glyphicon-star"></span>
+                    <span class="glyphicon glyphicon-star-empty"></span>
+                    4.0 stars
+                </p>
+              </div>
+            </div>
 
-<div id="footer"><?php include_once 'footer.php'; ?></div>
+            <div class="well">
 
-<script src="shopList.js"></script>
+                <div class="text-right">
+                    <a class="btn btn-success">Leave a Review</a>
+                </div>
+
+                <hr>
+
+                <div class="row">
+                    <div class="col-md-12">
+                        <span class="glyphicon glyphicon-star"></span>
+                        <span class="glyphicon glyphicon-star"></span>
+                        <span class="glyphicon glyphicon-star"></span>
+                        <span class="glyphicon glyphicon-star"></span>
+                        <span class="glyphicon glyphicon-star-empty"></span>
+                        Anonymous
+                        <span class="pull-right">10 days ago</span>
+                        <p>This product was great in terms of quality. I would definitely buy another!</p>
+                    </div>
+                </div>
+
+                <hr>
+
+                <div class="row">
+                    <div class="col-md-12">
+                        <span class="glyphicon glyphicon-star"></span>
+                        <span class="glyphicon glyphicon-star"></span>
+                        <span class="glyphicon glyphicon-star"></span>
+                        <span class="glyphicon glyphicon-star"></span>
+                        <span class="glyphicon glyphicon-star-empty"></span>
+                        Anonymous
+                        <span class="pull-right">12 days ago</span>
+                        <p>I've alredy ordered another one!</p>
+                    </div>
+                </div>
+
+                <hr>
+
+                <div class="row">
+                    <div class="col-md-12">
+                        <span class="glyphicon glyphicon-star"></span>
+                        <span class="glyphicon glyphicon-star"></span>
+                        <span class="glyphicon glyphicon-star"></span>
+                        <span class="glyphicon glyphicon-star"></span>
+                        <span class="glyphicon glyphicon-star-empty"></span>
+                        Anonymous
+                        <span class="pull-right">15 days ago</span>
+                        <p>I've seen some better than this, but not at this price. I definitely recommend this item.</p>
+                    </div>
+                </div>
+
+            </div>
+
+          </div>
+
+      </div>
+
+  </div>
+  <!-- /.container -->
+  
+  <div id="footer"><?php include_once 'footer.php'; ?></div>
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script>window.jQuery || document.write('<script src="js/vendor/jquery-3.1.1.min.js"><\/script>')</script>
+
+  <script src="js/vendor/bootstrap.min.js"></script>
+
+  <script src="js/main.js"></script>
+
+  <script src="shopList.js"></script>
 
 </body>
 
