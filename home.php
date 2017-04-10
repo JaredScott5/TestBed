1a7459b89ba7b6e463a85ab6f8022b1ff8a66bda
root <jmsmm92@gmail.com>
2017-04-10 18:47:59 -0500

    more testing git... replaced tabs with 2x spaces after pulling from git

--- home.php
+++ home.php
@@ -2,13 +2,18 @@
   session_start();
   include_once 'dbconnect.php';
 
-if(!isset($_SESSION['userSession']))
-{
- header("Location: index.php");
-}
+  if(!isset($_SESSION['userSession']))
+  {
+   header("Location: index.php");
+  }
 
-$query = $MySQLi_CON->query("SELECT * FROM users WHERE user_id=".$_SESSION['userSession']);
-$userRow=$query->fetch_array();
+  $query = $MySQLi_CON->query(
+    "SELECT * 
+     FROM users 
+     WHERE user_id=".$_SESSION['userSession']
+  );
+
+  $userRow=$query->fetch_array();
 
 $result = $MySQLi_CON->query("
 SELECT item_id, image, itemName, price, description, DATE(dateAdded) 
